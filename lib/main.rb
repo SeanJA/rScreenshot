e1f0bd569d56336e638f19de194cc4075e68e1cf
SeanJA <sean.sandy@gmail.com>
2010-09-29 23:44:36 -0300

    rScreenShot not ScreenShot

--- lib/main.rb
+++ lib/main.rb
@@ -1,7 +1,7 @@
 require 'tray_application';
 require 'screen_shot';
 
-app = TrayApplication.new("Screenshot")
+app = TrayApplication.new("rScreenShot")
 app.icon_filename = 'camera-web.png'
 app.item('Take Screenshot'){Screenshot.capture}
 app.item('Exit'){java.lang.System::exit(0)}
